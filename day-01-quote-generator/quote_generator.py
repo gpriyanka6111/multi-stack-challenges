8831102f75371e8bc5f950c1ac1ea55c79606729
Priyanka <gpriyanka6111@gmail.com>
2026-01-19 21:54:08 -0500

    Basic Quote generator

--- day-01-quote-generator/quote_generator.py
+++ day-01-quote-generator/quote_generator.py
@@ -1,1 +1,8 @@
-
+import random
+quotes = ["The secret of getting ahead is getting started.", 
+          "Staying consistent will give better results",
+          "Code every day, even if it's just 10mins.",
+          "Progress over perfection.",
+          "Small daily improvements lead to stunning results."]
+random_qoute = random.choice(quotes)
+print(random_qoute)
